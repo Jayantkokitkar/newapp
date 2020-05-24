e9d10a7ad566bd19b5c184beb38f1ea3b7a8e38b
Jayantkokitkar <kokitkarjayant@gmail.com>
2020-05-24 17:12:07 +0530

    Updates

---
+++ precache-manifest.ac383c14dffb751ca7e581eb3e8f8d8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4411fb96449ccea3eec434fe1b7fa1",
+    "url": "/newapp/index.html"
+  },
+  {
+    "revision": "17bbb17f81d0644eb41f",
+    "url": "/newapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/newapp/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newapp/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17bbb17f81d0644eb41f",
+    "url": "/newapp/static/js/main.0a3ea8b9.chunk.js"
+  },
+  {
+    "revision": "9a76567c649f391b1a5f",
+    "url": "/newapp/static/js/runtime-main.655584f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
